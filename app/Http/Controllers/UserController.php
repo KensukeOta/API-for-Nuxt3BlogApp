c83110a708b3d82183f53615014d6ac60cc4f1c0
Kensuke Ota <40653830+KensukeOta@users.noreply.github.com>
2023-10-27 21:52:36 +0900

    ユーザー登録機能のAPIを実装 (#2)

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\JsonResponse;
+
+class UserController extends Controller
+{
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'password_confirmation' => Hash::make($request->password_confirmation),
+        ]);
+
+        return response()->json(['user' => $user], 201);
+    }
+}
