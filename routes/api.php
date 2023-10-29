39fee1e7aefdf6b3928295a1d17c63cdb3081803
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-10-29 18:05:39 +0900

    記事のデータを保存するためのPostモデルとpostsテーブルを実装 (#5)

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json(['authUser' => $request->user()]);
 });
 
 Route::controller(UserController::class)->group(function () {
