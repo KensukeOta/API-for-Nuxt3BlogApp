9a32c07dfec9afc832423a31c621137cd5cdc582
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-10-29 18:17:20 +0900

    PostRequestを作成し、バリデーションのロジックの一元管理とバリデーションメッセージの日本語化を実装 (#5)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
 class PostController extends Controller
 {
-    public function store(Request $request): JsonResponse
+    public function store(PostRequest $request): JsonResponse
     {
         $post = Post::create([
             'title' => $request->title,
